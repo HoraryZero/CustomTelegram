1b75e3bddf9869603552a81474fb46b9fc9715c5
horaryzero <rnd.programmer.mike@gmail.com>
2020-06-02 15:09:21 +0300

    Preparing a project for development

--- app/src/main/java/com/example/customtelegram/MainActivity.kt
+++ app/src/main/java/com/example/customtelegram/MainActivity.kt
@@ -2,6 +2,10 @@ package com.example.customtelegram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.customtelegram.databinding.ActivityMainBinding
+
+// Bunch
+private lateinit var mBinding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
